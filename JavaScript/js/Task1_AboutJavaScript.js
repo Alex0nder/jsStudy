eb2c5e11c09f5d75b8882104fc02a3fa929da122
Alexander <alex0nder.y@gmail.com>
2023-06-10 06:11:36 +0300

    about javascript

---
+++ JavaScript/js/Task1_AboutJavaScript.js
@@ -0,0 +1,36 @@
+//Javascript = Expressions, functions, objects
+//all entities in js = objects
+//Object - property set
+//"Name: value"
+
+//Exemple
+
+// {                                close brace
+//     visible: true,               value & properties
+//     colorDepth: 24,              value & properties
+//     title: 'My Image',           value & properties
+//     orientation: {               nested objects and open brace
+//         angle: 0,                nested objects
+//         type: 'landscape'        nested objects
+//     },                           close brace
+//     pixeLDepth: 24,              value & properties
+//     width: 1440                  value & properties
+// }                                close brace
+
+// Array is an object
+// Function is an object
+// Number is an object
+// String is an object
+
+// console.log('Hello World')
+// Console - object
+//. - sintaksis
+// Log - method. A function that is the value of an object
+// () - Method call
+// 'Hello World' - Value type String (we can use ' ' or " ")
+
+//console.dir() - we can show all objects
+//console.table() - all value in table
+
+console.log('Alex')
+//undefined - Value - console.log done task show in log <- from js
