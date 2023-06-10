2ff5d49dc61946714754bec4a8e7471f518f5222
Alexander <alex0nder.y@gmail.com>
2023-06-10 19:39:17 +0300

    JS - it's Objects

--- JavaScript/js/Task1_AboutJavaScript.js
+++ JavaScript/js/Task1_AboutJavaScript.js
@@ -32,5 +32,100 @@
 //console.dir() - we can show all objects
 //console.table() - all value in table
 
-console.log('Alex')
+// console.log('Alex')
 //undefined - Value - console.log done task show in log <- from js
+
+
+// Expressions
+// 'abc'                - String                    = abc
+// 10                   - Number                    = 10  
+// 5 + 2                - Addition operator         = 7
+// с = 10               - Assignment operator       10 = c, c = 10
+// 'Good ' + 'Evening'  - String concatenation      = Good Evening
+// a <= b || c !== d    - multiple operators        = true or false
+// myFunction(c, d)     - Calling Functions         = Value result
+
+// Assignment operator
+// a = 20
+
+// Expressions with side effects - Not only returns a value, but also performs other actions
+// a = 5
+// b++
+// myFunction(c, d)
+
+// Variables - Re-accessing a value
+// с = 10
+// с + 5 = 15
+
+// Name Variables
+// PascalCase    - TypeClasses
+// DB_PASSWORD   - value before launch
+// camelCase     - All other variables
+// Variable names should be clear
+
+// Declaring Variables
+// let              let a               Declaration
+// const            const c = 10        Declaration and assignment
+// var              a = true            Assignment
+
+// let
+// let a = 10       Creat a=10
+// a = 20           New a=20
+// let b            Before assigment
+// b = false        Assignment
+
+// const
+// const c = 10     Creat c=10          Creat new Assignment
+// c = 20 - TypeError: Assignment to cinstant variable
+// If you don't plan to assign new values then use — const
+
+// Declarations and assignments
+// console.log(a)   ReferenceError: a s not defined
+// let a            Declarations
+// console.log(a)   Undefined
+// a = true         Assignments
+// console.log(a)   true
+
+// let myName
+
+// console.log(myName)
+
+// myName = "Alex"
+
+// console.log(myName)
+
+// console.log(console.log(10))
+
+// const myName = 'Alex'
+// myName = ''
+// console.log(myName)
+
+// const a = 10        //Number
+// const b = 'abc'     //String
+
+
+// Primitive Types and Reference Type
+// variable => have values => have type
+
+// Primitive Types
+// String       'Alex'          Only string
+// boolean      true, false     logic
+// number       -5, 0, 3, 4     number
+// null         null            only null
+// undefuned    undefuned       no value
+// symbol       &#169           symol for unical value
+
+
+// Reference type
+// "Hello world"
+// undefined
+// true
+// 25
+
+//Object ling
+// 0x3151
+// {a:10, b:10}
+
+//Object Array
+//0x7213
+// [1, 2, 3]
