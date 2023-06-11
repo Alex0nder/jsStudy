6dd827c61b7ca6e8e282cad48c8c1d3e550efaea
Alexander <alex0nder.y@gmail.com>
2023-06-12 02:43:43 +0300

    about JSON format

---
+++ JavaScript/js/Task8_JSON.js
@@ -0,0 +1,25 @@
+// JavaScript object notation
+
+// {
+//     "userId": 1,                 // Number
+//     "id": 1,                     // Number
+//     "title": "Test title",       // String
+//     "status": {                  // Nested object
+//         "completed": false       // Logic
+//     }
+// }
+
+// Pars JSON
+// JSON.parese()
+
+// {
+//     userId: 1,
+//     id: 1,
+//     title: 'Test title',
+//     status: {
+//         copleted: false,
+//     },
+// }
+
+// Converter JS to JSON
+//JSON.stringify()
